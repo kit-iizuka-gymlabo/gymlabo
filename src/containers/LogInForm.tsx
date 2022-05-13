0fcf01aa76021893ea206019319480eed99279d6
takatsutaichi <networktkd11@gmail.com>
2022-05-13 23:07:57 +0900

    update: alert to another file

--- src/containers/LogInForm.tsx
+++ src/containers/LogInForm.tsx
@@ -1,50 +1,18 @@
-import { FC, useRef } from 'react'
+import { FC } from 'react'
 import { useForm } from 'react-hook-form'
-import { useAuthContext } from '@/containers/AuthProvider'
 import Form from '@/components/organisms/Form'
 import FormLink from '@/components/atoms/FormLink'
+import FormAlert from '@/containers/FormAlert'
 import useAuth from '@/hooks/useAuth'
 import { FormInput } from '@/types/interfaces/Form'
-import {
-  AlertDialog,
-  AlertDialogBody,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogContent,
-  AlertDialogOverlay,
-  Button
-} from '@chakra-ui/react'
 
 const LogInForm: FC = () => {
-  const cancelRef = useRef(null)
-  const { handleLogIn, onClose } = useAuth()
-  const { user } = useAuthContext()
+  const { handleLogIn } = useAuth()
   const { register, handleSubmit, formState } = useForm<FormInput>()
-  const isLoggedIn = !!user
 
   return (
     <>
-      <AlertDialog
-        isOpen={isLoggedIn}
-        leastDestructiveRef={cancelRef}
-        onClose={onClose}
-      >
-        <AlertDialogOverlay>
-          <AlertDialogContent>
-            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
-              注意
-            </AlertDialogHeader>
-            <AlertDialogBody>
-              既にログインしています 
-            </AlertDialogBody>
-            <AlertDialogFooter>
-              <Button ref={cancelRef} onClick={onClose}>
-                トップに戻る
-              </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
+      <FormAlert />
       <Form
         register={register}
         handleSubmit={handleSubmit}
