9472b4326edd229d026062e79be02f5efa681b18
takatsutaichi <networktkd11@gmail.com>
2022-05-13 22:53:37 +0900

    update: Poster to have PosterModal

--- src/containers/Poster.tsx
+++ src/containers/Poster.tsx
@@ -1,10 +1,14 @@
 import { FC } from 'react'
 import { useGLTF } from '@react-three/drei'
+import { useDisclosure } from '@chakra-ui/react'
 import PosterProps from '@/types/interfaces/Poster'
 import Model from '@/components/molecules/Model'
+import PosterModal from '@/components/organisms/PosterModal'
+import ChakraWrapper from '@/components/atoms/ChakraWrapper'
 
-const Poster: FC<PosterProps> = ({ modelPath, onOpen }) => {
+const Poster: FC<PosterProps> = ({ modelPath }) => {
   const gltf = useGLTF(modelPath)
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   return (
     <>
@@ -12,6 +16,12 @@ const Poster: FC<PosterProps> = ({ modelPath, onOpen }) => {
         onClick={onOpen}
         gltf={gltf}
       />
+      <ChakraWrapper>
+        <PosterModal
+          isOpen={isOpen}
+          onClose={onClose}
+        />
+      </ChakraWrapper>
     </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,16 @@
-import { Suspense } from 'react'
 import { NextPage } from 'next'
-import { useDisclosure } from '@chakra-ui/react'
-import { useProgress } from '@react-three/drei'
 import path from '@/configs/model.json'
 import Poster from '@/containers/Poster'
-import PosterModal from '@/components/organisms/PosterModal'
 import DrawCanvas from '@/components/organisms/DrawCanvas'
-import Loader from '@/components/organisms/Loader'
 
-
-const IndexPage: NextPage = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const { progress } = useProgress()
-
-  return (
-    <>
-      <PosterModal
-        isOpen={isOpen}
-        onClose={onClose}
-      />  
-      <DrawCanvas>
-        <Suspense fallback={<Loader progress={progress}/>}>
-          <Poster
-            modelPath={path.model_path}
-            onOpen={onOpen}
-          />
-        </Suspense>
-      </DrawCanvas>
-    </>
-  )
-}
+const IndexPage: NextPage = () => (
+  <>
+    <DrawCanvas>
+      <Poster
+        modelPath={path.model_path}
+      />
+    </DrawCanvas>
+  </>
+)
 
 export default IndexPage
