c9293e898e54a6089e70ea5be6556d7fc4e35ea5
takatsutaichi <networktkd11@gmail.com>
2022-05-13 10:54:03 +0900

    add: firebase

---
+++ src/utils/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp, FirebaseOptions, FirebaseApp } from 'firebase/app'
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: 'AIzaSyDzF3T8evUhiLy6awDBHoJVgyTtBuvfdZo',
+  authDomain: 'gymlabo.firebaseapp.com',
+  projectId: 'gymlabo',
+  storageBucket: 'gymlabo.appspot.com',
+  messagingSenderId: '770214888689',
+  appId: '1:770214888689:web:5041d3abcf0d97ed5c9550',
+  measurementId: 'G-TCX7RJ86ML'
+}
+
+export const app: FirebaseApp = initializeApp(firebaseConfig)
