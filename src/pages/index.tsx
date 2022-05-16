349367101691ced35242fbf2f276dc311771042a
takatsutaichi <networktkd11@gmail.com>
2022-05-16 16:49:39 +0900

    update: only index to have header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { NextPage } from 'next'
 import path from '@/configs/model.json'
 import Poster from '@/containers/Poster'
+import Header from '@/containers/Header'
 import DrawCanvas from '@/containers/DrawCanvas'
 
 const IndexPage: NextPage = () => (
   <>
+    <Header />
     <DrawCanvas>
       <Poster
         modelPath={path.model_path}
